d526cc629bdcef39ddf4d663edadff76c98640b9
frarosrod2 <pacorosa99@gmail.com>
2022-03-09 00:01:33 +0100

    Advances

--- angular-testing/src/tests/basic/counter/counter.component.spec.ts
+++ angular-testing/src/tests/basic/counter/counter.component.spec.ts
@@ -5,7 +5,7 @@ import { CounterComponent } from '../../../app/basic/counter/counter.component';
 describe('CounterComponent', () => {
   let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
-  let compile: HTMLElement;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,18 +17,32 @@ describe('CounterComponent', () => {
     fixture = TestBed.createComponent(CounterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   test('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  test('debe de hacer match', () => {
-    expect(compile).toMatchSnapshot();
-  });
-
   test('increaseBy increment', () => {
     component.increaseBy(5);
     expect(component.counter).toBe(15);
+  });
+
+  test('hacer click debe incrementar y decrementar en 1', () => {
+    const buttons = compiled.querySelectorAll('button');
+    buttons[0].click();
+    expect(component.counter).toBe(11);
+
+    buttons[1].click();
+    expect(component.counter).toBe(10);
+  });
+
+  test('cambiar counter debe actualizar etiqueta', () => {
+    component.increaseBy(10);
+    fixture.detectChanges();
+    
+    const h1 = compiled.querySelector('h1');
+    expect(h1?.textContent).toContain('20');
   });
 });
